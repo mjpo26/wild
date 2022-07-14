394830881972482e2c73d05f4ec6022bd148d81a
ad_mjkim <mj.kim@adbank.co.kr>
2022-07-14 14:52:23 +0900

    config update

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_w7YxcOR7w', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5p6eqsjrpg51dhrsbl40j73o4q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
